fd26f8ab29871f8eac65eb6498e9979d7b046fcb
Abdi Ahmed <abdi.cg.ahmed@gmail.com>
2015-06-18 15:56:37 +0100

    email functionality working but update to db to set validate to true not working

--- mandrill.js
+++ mandrill.js
@@ -3,21 +3,21 @@ var mandrill_client = new mandrill.Mandrill(process.env.SECRET);
 
 var email = {}; 
 
-email.sendEmail = function (email) {
+email.sendEmail = function (userObj) {
 	var data = {
 
 
 	      	'from_email': 'abdicagaros@gmail.com',
 	      	'to': [
 	          {
-	            'email': email,
-	            'name': 'Abdi',
+	            'email': userObj.email,
+	            'name': userObj.firstname,
 	            'type': 'to'
 	          }
 	        ],
 		      'autotext': 'true',
 		      'subject': 'Thanks for signing up',
-		      'html': 'YOu have now signed up to <h1>ranstagram</h1> app'
+		      'html': '<p>Thank you '+ userObj.firstname +' have now signed up to ranstagram app on ' +userObj.signUpTime+'. Please validate your email by clicking the following link:</p><a href="localhost:8000/validate/'+ userObj._id+'">Validate</a>'
 	};
 	mandrill_client.messages.send({"message": data, "async": false},function(result) {
 		console.log(result);
@@ -27,5 +27,3 @@ email.sendEmail = function (email) {
 }
 
 module.exports = email; 
-
-
