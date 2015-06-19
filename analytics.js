47021f850bea99a4d415006e3a0631ac30ba75d8
Lukars <lukas.siebeck@gmx.de>
2015-06-19 05:53:31 +0100

    analytics 1.1

--- analytics.js
+++ analytics.js
@@ -5,7 +5,10 @@ var analytics = {
 	readRequireLog: function (object, callback){	
 		var obj=[];
 		var counter = {};
+		var logVisitors = {};
 		var returnString = '<h1>Site call counter</h1>';
+		var returnUserString = '<p></p><h1>Unique visitors counter</h1>';
+		var theEnd = '';
 		fs.readFile(__dirname +'/src/log/response_log', 'utf8', function (err, data) {//derp-fault: switched arguments -> wasted 3,5h
 	  		if (err) throw err;
 
@@ -25,13 +28,38 @@ var analytics = {
 				else {
 					counter[a.path]++;
 				}
+				if (!logVisitors[a.source.remoteAddress]){
+					logVisitors[a.source.remoteAddress] = a.timestamp;
+					console.log("new Visitor registered");
+				}
+				else {
+					if (logVisitors[a.source.remoteAddress] > a.timestamp - 300000 ){
+					logVisitors[a.source.remoteAddress] = a.timestamp;
+					console.log("replacing timestamp, last click less than 30 minutes ago");
+					}
+					else {
+					logVisitors[a.source.remoteAddress] += "," +a.timestamp;
+					console.log("adding timestamp, last click more than 30 minutes ago!!!");						
+					}
+				}
 			}
+
+			for(var user in logVisitors){
+				if (logVisitors.hasOwnProperty(user)){
+					var times = logVisitors[user].split(',');
+					returnUserString += '<p><strong> Ip-adress: ' + user + '</strong> has accessed picup.com <i>' + times.length + '</i> times</p>';
+					// var p = logVisitors[a.source.remoteAddress];
+					// console.log(p);
+				}
+			}
+
 			for(var key in counter){
 				if (counter.hasOwnProperty(key)){
 					returnString += '<p><strong>www.picup.com' + key + '</strong> has been accessed <i>'+ counter[key] +'</i> times.</p>';
 				}
 			}
-			callback(returnString,null);
+			theEnd = returnString + returnUserString;
+			callback(theEnd,null);
 		});
 	},
 
